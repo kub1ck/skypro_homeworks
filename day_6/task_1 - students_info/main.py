18851fbf3724f842ea9764ed9deedb62ebd0b220
Daniil <daniil77089@gmail.com>
2022-09-15 12:43:03 +0300

    main.py and functions.py update

--- day_6/task_1 - students_info/main.py
+++ day_6/task_1 - students_info/main.py
@@ -1,18 +1,17 @@
 import functions
 
-# Удобные словари для работы
-students = functions.get_students_dict()
-professions = functions.get_professions_dict()
+STUDENTS = functions.load_students()
+PROFESSIONS = functions.load_professions()
 
 
-def check_fitness(student: dict, profession: list) -> dict:
+def check_fitness(student: dict, profession: dict) -> dict:
     """
     Формируем словарь с информацией о пригодности студента на специальность.
     """
 
-    student_has = set(profession).intersection(set(student['skills']))
-    student_lacks = set(profession).difference(set(student['skills']))
-    student_fitness = int(len(student_has) / len(profession) * 100)
+    student_has = set(profession['skills']).intersection(set(student['skills']))
+    student_lacks = set(profession['skills']).difference(set(student['skills']))
+    student_fitness = int(len(student_has) / len(profession['skills']) * 100)
 
     result = {
         "has": list(student_has),
@@ -23,44 +22,38 @@ def check_fitness(student: dict, profession: list) -> dict:
     return result
 
 
-def check_title(title: str) -> bool:
-    """
-    Проверяем, есть ли специальность по названию.
-    """
-
-    return title in professions.keys()
-
-
-def check_pk(pk: int) -> bool:
-    """
-    Проверяем, имеется ли пользователь с таким номером.
-    """
-
-    return pk in students.keys()
-
-
 def main() -> None:
     # Работа с информацией пользователя
+    pks = []
+
+    for student in STUDENTS:
+        pks.append(student['pk'])
+
     pk = int(input("Введите номер пользователя: "))
 
-    if not check_pk(pk):
+    if pk not in pks:
         print("У нас нет такого студента")
         quit()
 
-    student_info = functions.get_student_by_pk(pk)
+    student_info = functions.get_student_by_pk(pk, STUDENTS)
     student_skills = ", ".join(student_info['skills'])
-    print(f"Студент {student_info['name']}",
+    print(f"Студент {student_info['full_name']}",
           f"Знает: {student_skills}",
           sep='\n')
 
     # Работа с информацией специальностей
+    titles = {}
+
+    for i in range(len(PROFESSIONS)):
+        titles[PROFESSIONS[i]['title']] = i + 1
+
     title = input("\nВыберите специальность для оценки студента: ")
 
-    if not check_title(title):
+    if title not in titles.keys():
         print("У нас нет такой специальности")
         quit()
 
-    profession_info = functions.get_profession_by_title(title)
+    profession_info = functions.get_profession_by_title(title, titles, PROFESSIONS)
 
     # Работа с пригодностью
     student_fitness = check_fitness(student_info, profession_info)
@@ -68,8 +61,8 @@ def main() -> None:
     student_lacks = ", ".join(student_fitness['lacks'])
 
     print(f"\nПригодность: {student_fitness['fit_percent']}%",
-          f"{student_info['name']} знает: {student_has}",
-          f"{student_info['name']} не знает: {student_lacks}",
+          f"{student_info['full_name']} знает: {student_has}",
+          f"{student_info['full_name']} не знает: {student_lacks}",
           sep='\n')
 
 
